4eec76606b8eb4019bf4e24662844b322796184a
Calendar User <user@example.com>
2026-07-13 10:44:57 +0800

    Fix: merge migration heads (image_urls + layout_configs)

---
+++ migrations/versions/c56e6b2a8a57_merge_image_urls_and_layout_configs.py
@@ -0,0 +1,24 @@
+"""merge_image_urls_and_layout_configs
+
+Revision ID: c56e6b2a8a57
+Revises: 3a9d8c4e72b1, 6d9c7a1b2f10
+Create Date: 2026-07-13 10:44:45.576185
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c56e6b2a8a57'
+down_revision = ('3a9d8c4e72b1', '6d9c7a1b2f10')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
